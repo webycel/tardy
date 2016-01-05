bea25baf51a1a86e625f781bc53c6dfcdcb7a5f1
webycel <anthony.web91@gmail.com>
2016-01-05 15:22:47 +0000

    init css/js setup and plugins

--- app/js/app.js
+++ app/js/app.js
@@ -1,24 +1,18 @@
 'use strict';
 
-var app = (function(document, $) {
+var Tardy = (function(document, $) {
 	var docElem = document.documentElement,
 		_userAgentInit = function() {
 			docElem.setAttribute('data-useragent', navigator.userAgent);
 		},
 		_init = function() {
-		// 	$(document).foundation();
-    //         // needed to use joyride
-    //         // doc: http://foundation.zurb.com/docs/components/joyride.html
-    //         $(document).on('click', '#start-jr', function () {
-    //             $(document).foundation('joyride', 'start');
-    //         });
-		// 	_userAgentInit();
+			
 		};
 	return {
 		init: _init
 	};
-})(document, jQuery);
+})(document, Zepto);
 
 (function() {
-	app.init();
+	Tardy.init();
 })();
